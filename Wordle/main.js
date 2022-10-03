109c91f83188007d095aa090253a8af6ec982a86
Sebastian Bogensperger <sebastian.bogensperger@hotmail.com>
2022-10-03 17:39:48 +0200

    klick keyboard fix

--- Wordle/main.js
+++ Wordle/main.js
@@ -9,33 +9,34 @@ const dict_words = setDict();
 const solution_word_array = setSolutions();
 const logger = new SimpleLogger(LoggerLevel.OFF);
 /*START MAIN LOGIC*/
-{
-    const element = window.document;
-    let Board = new GameBoard(element, logger);
-    let MyGame = new WordleGame(Board, dict_words, logger);
-    MyGame.newGame(LENGTH_WORD, solution_word_array);
-    document.addEventListener('keyup', (event) => {
-        let name = event.key;
-        if (logger.level >= LoggerLevel.INFO) {
-            console.log(`Event \'keyup\' ${event}->${name}`);
-        }
-        if (name == 'End') {
-            logger.level =
-                logger.level === LoggerLevel.OFF
-                    ? LoggerLevel.INFO
-                    : LoggerLevel.OFF;
-            console.log(`Logger level: ${logger.level}`);
-        }
-        else if (MyGame.gameState !== GameState.FINISHED ||
-            name === 'Enter' ||
-            name === 'Dead') {
-            MyGame.inputSingleLetter(name);
-        }
-    }, false);
-    function clickLetter(letter) {
-        MyGame.inputSingleLetter(letter);
+const element = window.document;
+let Board = new GameBoard(element, logger);
+let MyGame = new WordleGame(Board, dict_words, logger);
+MyGame.newGame(LENGTH_WORD, solution_word_array);
+document.addEventListener('keyup', (event) => {
+    let name = event.key;
+    if (logger.level >= LoggerLevel.INFO) {
+        console.log(`Event \'keyup\' ${event}->${name}`);
     }
-}
+    if (name == 'End') {
+        logger.level =
+            logger.level === LoggerLevel.OFF
+                ? LoggerLevel.INFO
+                : LoggerLevel.OFF;
+        console.log(`Logger level: ${logger.level}`);
+    }
+    else if (MyGame.gameState !== GameState.FINISHED ||
+        name === 'Enter' ||
+        name === 'Dead') {
+        MyGame.inputSingleLetter(name);
+    }
+}, false);
+document.addEventListener('click', function handleClick(event) {
+    let target = event.target;
+    if (target.classList.contains('keyboard_letter')) {
+        MyGame.inputSingleLetter(target.id);
+    }
+});
 /*Setup Helper Functions */
 function setVersion(version) {
     let elem = document.getElementById('version');
--- Wordle/main.ts
+++ Wordle/main.ts
@@ -18,43 +18,45 @@ const solution_word_array = setSolutions();
 const logger = new SimpleLogger(LoggerLevel.OFF);
 
 /*START MAIN LOGIC*/
-{
-    const element = window.document;
 
-    let Board: GameBoardInterface = new GameBoard(element, logger);
-    let MyGame = new WordleGame(Board, dict_words, logger);
+const element = window.document;
 
-    MyGame.newGame(LENGTH_WORD, solution_word_array);
+let Board: GameBoardInterface = new GameBoard(element, logger);
+let MyGame = new WordleGame(Board, dict_words, logger);
 
-    document.addEventListener(
-        'keyup',
-        (event) => {
-            let name = event.key;
-            if (logger.level >= LoggerLevel.INFO) {
-                console.log(`Event \'keyup\' ${event}->${name}`);
-            }
+MyGame.newGame(LENGTH_WORD, solution_word_array);
 
-            if (name == 'End') {
-                logger.level =
-                    logger.level === LoggerLevel.OFF
-                        ? LoggerLevel.INFO
-                        : LoggerLevel.OFF;
-                console.log(`Logger level: ${logger.level}`);
-            } else if (
-                MyGame.gameState !== GameState.FINISHED ||
-                name === 'Enter' ||
-                name === 'Dead'
-            ) {
-                MyGame.inputSingleLetter(name);
-            }
-        },
-        false,
-    );
+document.addEventListener(
+    'keyup',
+    (event) => {
+        let name = event.key;
+        if (logger.level >= LoggerLevel.INFO) {
+            console.log(`Event \'keyup\' ${event}->${name}`);
+        }
 
-    function clickLetter(letter: string) {
-        MyGame.inputSingleLetter(letter);
+        if (name == 'End') {
+            logger.level =
+                logger.level === LoggerLevel.OFF
+                    ? LoggerLevel.INFO
+                    : LoggerLevel.OFF;
+            console.log(`Logger level: ${logger.level}`);
+        } else if (
+            MyGame.gameState !== GameState.FINISHED ||
+            name === 'Enter' ||
+            name === 'Dead'
+        ) {
+            MyGame.inputSingleLetter(name);
+        }
+    },
+    false,
+);
+
+document.addEventListener('click', function handleClick(event) {
+    let target = event.target as HTMLElement;
+    if (target.classList.contains('keyboard_letter')) {
+        MyGame.inputSingleLetter(target.id);
     }
-}
+});
 
 /*Setup Helper Functions */
 function setVersion(version: string) {
